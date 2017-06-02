397671aca2b38ce1168acdc3a4e387ff4411b305
Michal Rutka <michal.rutka@gmail.com>
2017-06-02 16:37:15 +0200

    Changed image to aktur/crashplan

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t jrcs/crashplan:dev . && \
-docker push jrcs/crashplan:dev
+docker build -t aktur/crashplan . && \
+docker push aktur/crashplan
---
+++ run.sh
@@ -0,0 +1,6 @@
+# Based on https://miketabor.com/run-crashplan-docker-synology-nas/
+sudo docker echo run -d --name Crashplan \
+--restart unless-stopped \
+--net=host \
+--volume /volume1:/volume1 \
+--volume `pwd`/config:/var/crashplan aktur/crashplan:latest
